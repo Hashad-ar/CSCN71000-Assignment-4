956b9124924be833788fe4e90486858522cf146b
Abdelrahman Hashad <Ahashad3286@conestogac.on.ca>
2021-12-11 19:19:38 -0500

    Full game completed with 2 more functions added

--- CSCN71000-Assignment 4/Source.c
+++ CSCN71000-Assignment 4/Source.c
@@ -1,46 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
-int ValidateInput(int input);
-int GuessCheck(int magicnumber, int guessnumber);
-int input(void);
 
 int main(void)
 {
-	return 0;
-}
-
-int input(void)
-{
-	int user_input = 0;
+	int MagicNum;
+	int result;
+	int guessresult = -1;
+	int GuessNum;
 
 	printf("Player1, enter the guessing magic number from 1 - 1000 for Player2 (Make sure they are not cheating :D)\n");
-
-	int result = scanf("%d\n", &user_input);
-
-	if (result == 1 && getchar() == '\n')
+	
+	do 
 	{
-		return user_input;
+		result = scanf("%d", &MagicNum);
+		result = validateInput(MagicNum, result);
+	} while (result != 1);
+	
+	system("cls");
+	printf("Now pass the game to Player2 to guess the MagicNum (You only got 6 tries) Good Luck!\n Enter a value from 1 - 1000\n");
+
+	if (guessresult == -1)
+	{
+		for (int incorrectguess = 1; incorrectguess <= 6; incorrectguess++)
+		{
+			result = scanf("%d", &GuessNum);
+			result = validateInput(GuessNum, result);
+			if (result == 0)
+			{
+				printf("X\n");					//Prints X if the guess number entered is an invalid incorrect
+			}
+			else
+			{
+				guessresult = guessCheck(MagicNum, GuessNum);
+			}
+		}
 	}
 	else
 	{
-		return 0; // False / Invalid
+		return 0;
 	}
-	ValidateInput(input);
-
 }
 
-int ValidateInput(int input)
+int validateInput(input, result)			//Validates the number the users' enter
 {
-	if (input < 1 || input > 1000)
-	{
-		printf("Invalid entry value should be a number from 1 - 1000, try again.");
-		return 0; // Invalid
-	}
-	else if (!(isdigit(input)))
+	 if (!(result == 1 && (isdigit(input)) || getchar(input) == '\n'))
 	{
 		printf("Invalid entry value should be a number, try again.\n");
+		return 0;
+	}
+	else if (input < 1 || input > 1000)
+	{
+		printf("Invalid entry value should be from 1 - 1000, try again.\n");
 		return 0;
 	}
 	else
@@ -49,31 +61,26 @@ int ValidateInput(int input)
 	}
 }
 
-int GuessCheck(int magicnumber, int guessnumber)
+int guessCheck(int magicnumber, int guessnumber)		//Checks if the magic number has been guessed
 {
-	for (int IncorrectGuess = 0; IncorrectGuess < 6;)
+	if (guessnumber < magicnumber)
 	{
-		int result1 = scanf("%d", &guessnumber);
-
-		if (guessnumber < magicnumber)
-		{
-			printf("Too Low!\n");
-			IncorrectGuess++;
-		}
-		else if (guessnumber > magicnumber)
-		{
-			printf("Too High!\n");
-			IncorrectGuess++;
-		}
-		else if (guessnumber == magicnumber)
-		{
-			printf("You won!");
-			return 1;
-		}
-		else
-		{
-			printf("You lost!");
-			return 0;
-		}
+		printf("Too Low!\n");
+		return -1;
+	}
+	else if (guessnumber > magicnumber)
+	{
+		printf("Too High!\n");
+		return -1;
+	}
+	else if (guessnumber == magicnumber)
+	{
+		printf("You won!");
+		return 1;
+	}
+	else
+	{
+		printf("You lost!");
+		return 0;
 	}
 }
